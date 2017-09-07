e19cbd4b8a36b190109af65406851a151c6df8d4
annalaise <annalaise.gibbons@gmail.com>
2017-09-07 17:32:16 +0100

    adding Play class

---
+++ lib/play.rb
@@ -0,0 +1,16 @@
+class Play
+
+  attr_reader :game
+
+  def initialize(game = Game.new)
+    @game = game
+  end
+
+
+
+  def print_board
+
+  end
+
+
+end
---
+++ spec/play_spec.rb
@@ -0,0 +1,9 @@
+require './lib/play.rb'
+
+describe Play do
+  subject(:play) { described_class.new }
+
+
+  
+
+end
